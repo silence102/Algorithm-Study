3d5947eb054bde25952559e5b68b1396486c393c
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-01-11 10:12:05 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.py
@@ -7,3 +7,8 @@ def solution(numer1, denom1, numer2, denom2):
     denom = denom // getgcd
     answer = [numer, denom]
     return answer
+# from fractions import Fraction
+# def solution(num1,den1,num2,den2):
+#     answer = Fraction(num1,den1) + Fraction(num2,den2)
+#     return [(answer.numerator),(answer.denominator) ]
+    
