653d697615cb45586c7e48c4cadcc0b0e121d023
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-01-30 21:07:43 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,9 @@
+def solution(num_list):
+    odd = []
+    even = []
+    for i in num_list:
+        if i % 2 == 0:
+            even.append(i)
+        else:
+            odd.append(i)
+    return [len(even),len(odd)]
