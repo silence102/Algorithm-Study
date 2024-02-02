e0b37e97547f3c72e3b4d9b0261f34085770a87a
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-02-02 09:13:04 +0900

    [Bronze V] Title: A/B, Time: 232 ms, Memory: 17976 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double X = scanner.nextDouble();
+        double Y = scanner.nextDouble();
+        
+        System.out.println(X / Y);
+    }
+}
