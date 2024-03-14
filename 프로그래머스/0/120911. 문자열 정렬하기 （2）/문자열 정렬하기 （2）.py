6439d851430bc1ad7385ac334b8f5986474755ff
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-03-14 18:06:15 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,6 +1,2 @@
 def solution(my_string):
-    answer = []
-    for i in my_string:
-        answer.append(i.lower())
-    answer.sort()
-    return ''.join(answer)
+    return "".join(sorted(my_string.lower()))
