5923a17437587b7abc6377b4fe71f44cef611a56
KimMinSeok <132821631+silence102@users.noreply.github.com>
2024-04-08 08:20:27 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        char A = scanner.nextLine().charAt(0);
+        int T = (int)A;
+        
+        System.out.println(T);
+    }
+}
