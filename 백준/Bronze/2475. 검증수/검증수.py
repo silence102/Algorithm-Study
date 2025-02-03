efb025999d9c177b3cfcfc18c5767021ff4f4926
KimMinSeok <132821631+silence102@users.noreply.github.com>
2025-02-03 19:38:29 +0900

    [Bronze V] Title: 검증수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -2,9 +2,9 @@ import sys
 
 def main():
     a, b, c, d, e = map(int, sys.stdin.readline().split())
-    total = a**2 + b**2 + c**2 + d**2 + e**2
-    code = total % 10
-    print(code)
+    total = a**2+b**2+c**2+d**2+e**2
+    result = total % 10
+    print(result)
     
 if __name__ == "__main__":
     main()
