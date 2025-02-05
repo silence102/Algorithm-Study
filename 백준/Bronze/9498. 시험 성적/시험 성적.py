e828cc33b4968a4d27e3a643b02bf058720baec3
KimMinSeok <132821631+silence102@users.noreply.github.com>
2025-02-05 20:27:31 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,18 @@
+import sys
+
+def main():
+    A = int(sys.stdin.readline())
+    if A >= 90 and A <= 100:
+        print("A")
+    elif A >= 80 and A <= 89:
+        print("B")
+    elif A >= 70 and A <= 79:
+        print("C")
+    elif A >= 60 and A <= 69:
+        print("D")
+    else:
+        print("F")
+
+    
+if __name__ == "__main__":
+    main()
