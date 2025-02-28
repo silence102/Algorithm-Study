546b96f0aa7885056f9895a277fbae5935763df2
KimMinSeok <132821631+silence102@users.noreply.github.com>
2025-02-28 17:26:31 +0900

    [Silver IV] Title: 제로, Time: 2552 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,13 @@
+K = int(input())
+
+stack = []
+
+for _ in range(K):
+    N = int(input())
+    if N == 0:
+        stack.pop()
+    else:
+        stack.append(N)
+
+result = sum(stack)
+print(result)
