54cd4ffe9e4972f61ae0cd57f41be185a31dcd86
KimMinSeok <132821631+silence102@users.noreply.github.com>
2025-03-20 00:20:27 +0900

    [Gold III] Title: 전국시대, Time: 156 ms, Memory: 44120 KB -BaekjoonHub

--- 백준/Gold/15809. 전국시대/전국시대.py
+++ 백준/Gold/15809. 전국시대/전국시대.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 def make_set(x):
     parents = [i for i in range(x + 1)]
     return parents
@@ -11,13 +14,13 @@ def find_set(x):
 def union_set(o, x, y):
     boss_x = find_set(x)
     boss_y = find_set(y)
+
+    if boss_x == boss_y:
+        return
     # 동맹 시그널
     if o == 1:
-        # 같은 동맹국 또는 속국일 때
-        if boss_x == boss_y:
-            return
         # 동맹 후 - 큰 규모 국가에 몰아주기
-        if world[boss_x] > world[boss_y]:
+        if boss_x > boss_y:
             world[boss_x] += world[boss_y]
             parent[boss_y] = boss_x
             world[boss_y] = 0
@@ -27,11 +30,7 @@ def union_set(o, x, y):
             world[boss_x] = 0
 
     # 전쟁 시그널
-    if o == 2:
-        # 아군과는 싸울 수 없음
-        if boss_x == boss_y:
-            return
-
+    else:
         # 주전 국 간의 규모 비교 3가지 경우
         if world[boss_x] > world[boss_y]:
             world[boss_x] -= world[boss_y]
@@ -53,8 +52,7 @@ parent = make_set(N)
 
 world = [0]
 for _ in range(N):
-    country = int(input())
-    world.append(country)
+    world.append(int(input()))
 
 for _ in range(M):
     # O = 1 (동맹), O = 2 (전쟁), P, Q 는 대상 국가
@@ -62,12 +60,7 @@ for _ in range(M):
     union_set(O, P, Q)
 
 remain_country = []
-# remain_country = set()
 remain_power = []
-
-# for i in range(1, N+1): 
-#     con_num = find_set(parent[i])
-#     remain_country.add(con_num)
 
 for i in world:
     if i != 0:
