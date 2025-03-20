1207880ae97849174c556a3fc6ea943cbe3d60c3
KimMinSeok <132821631+silence102@users.noreply.github.com>
2025-03-20 23:22:03 +0900

    [Silver II] Title: 예산, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -5,23 +5,18 @@ N = int(input())
 ask = list(map(int, input().split()))
 budget = int(input())
 
-max_v = max(ask)
+start = 0
+end = max(ask)
+answer = 0
 
-if sum(ask) <= budget:
-    print(max_v)
-else:
-    start = 0
-    end = max_v
-    answer = 0
+while start <= end:
+    mid = (start + end) // 2
+    total = sum(min(cost, mid) for cost in ask)
 
-    while start <= end:
-        mid = (start + end) // 2
-        total = sum(min(mid, cost) for cost in ask)
+    if total <= budget:
+        start = mid + 1
+        answer = mid
+    else:
+        end = mid - 1
 
-        if total <= budget:
-            start = mid + 1
-            answer = mid
-        else:
-            end = mid - 1
-
-    print(answer)
+print(answer)
