a17b580d447341f92040537d25020a14508931bb
KimMinSeok <132821631+silence102@users.noreply.github.com>
2025-03-25 07:28:22 +0900

    [Bronze V] Title: 오늘 날짜, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.py
@@ -1,9 +1,1 @@
-from datetime import datetime, timezone, timedelta
-
-def main():
-    utc_today = datetime.now(timezone.utc)
-    kst_today = utc_today + timedelta(hours=9)
-    print(kst_today.strftime("%Y-%m-%d"))
-    
-if __name__ == "__main__":
-    main()
+print("2025-03-25")
