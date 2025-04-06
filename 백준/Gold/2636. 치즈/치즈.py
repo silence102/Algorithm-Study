974159a5028101d833ea5661085db3056c7029a2
KimMinSeok <132821631+silence102@users.noreply.github.com>
2025-04-06 13:53:41 +0900

    [Gold IV] Title: 치즈, Time: 72 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.py
+++ 백준/Gold/2636. 치즈/치즈.py
@@ -1,58 +1,56 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
 dr = [-1, 1, 0, 0]
 dc = [0, 0, -1, 1]
 
-def bfs(s1, s2, step, total):
+def bfs():
     visited = [[0] * N for _ in range(M)]
     queue = deque()
-    cheese = set()
-    queue.append((s1, s2))
-    visited[s1][s2] = 1
+    melt = set()
+
+    queue.append((0, 0))
+    visited[0][0] = 1
 
     while queue:
         r, c = queue.popleft()
 
-        for k in range(4):
-            di = r + dr[k]
-            dj = c + dc[k]
+        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            di = r + dr
+            dj = c + dc
 
-            if 0 <= di < M and 0 <= dj < N and not visited[di][dj] and not matrix[di][dj]:
-                visited[di][dj] = 1
-                queue.append((di, dj))
-            elif 0 <= di < M and 0 <= dj < N and not visited[di][dj] and matrix[di][dj]:
-                cheese.add((di, dj))
+            if 0 <= di < M and 0 <= dj < N:
+                if not visited[di][dj]: 
+                    visited[di][dj] = 1
+                    if not matrix[di][dj]:
+                        queue.append((di, dj))
+                    elif matrix[di][dj]:
+                        melt.add((di, dj))
 
-    melt = []
-    cheese = deque(cheese)
-    remain = len(cheese)
-
-    while cheese:
-        r, c = cheese.popleft()
-
-        for k in range(4):
-            di = r + dr[k]
-            dj = c + dc[k]
-
-            if visited[di][dj]:
-                melt.append((r,c))
-                break
-    
     if not melt:
-        return [step, total]
+        return 0
     
-    for i in melt:
-        matrix[i[0]][i[1]] = 0
+    for x, y in melt:
+        matrix[x][y] = 0
 
-    return bfs(0, 0, step + 1, remain)
-
+    return len(melt)
 
 
 M, N = map(int, input().split())
-
 matrix = [list(map(int, input().split())) for _ in range(M)]
 
-result = bfs(0, 0, 0, 0)
+step = 0
+last_cheese = 0
 
-print(result[0])
-print(result[1])
+while True:
+    end = bfs()
+
+    if end == 0:
+        break
+
+    last_cheese = end
+    step += 1
+
+print(step)
+print(last_cheese)
