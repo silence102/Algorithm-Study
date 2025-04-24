f5889674ca5e1b5358baa7eff1a99cf311821149
KimMinSeok <132821631+silence102@users.noreply.github.com>
2025-04-24 21:01:37 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,9 @@
+N, M = map(int, input().split())
+
+nums = list(range(1, N+1))
+for _ in range(M):
+    i, j = map(int, input().split())
+
+    nums[i-1], nums[j-1] = nums[j-1], nums[i-1]
+
+print(*nums)
